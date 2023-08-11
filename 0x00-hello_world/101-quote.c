3afd9db18f3e0a64aab3efe4208e53773a56f06d
EmeanaDaniel <emeanadaniel@gmail.com>
2023-08-10 17:41:09 -0700

    c programing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+gcc -S -masm=intel $CFILE
+[8/10, 11:16 PM] +39 351 149 9377: #include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+        write(2,
+        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
+}
