f62800c9250aba13268e388894145014a3d564a7
EmeanaDaniel <emeanadaniel@gmail.com>
2023-08-10 18:06:08 -0700

    c programing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,11 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
-[8/10, 11:16 PM] +39 351 149 9377: #include <stdio.h>
-#include <unistd.h>
-
+#include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-        write(2,
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
